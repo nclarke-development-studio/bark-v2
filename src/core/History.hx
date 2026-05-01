0a60d2cf1f61cd6b5728b92ec631d8d4bc91acb7
Neil <ntclarke33@gmail.com>
2026-05-01 12:20:59 +0200

    feat: history cap

--- src/core/History.hx
+++ src/core/History.hx
@@ -5,12 +5,18 @@ import core.commands.ICommand;
 class History {
 	var undoStack:Array<ICommand> = [];
 	var redoStack:Array<ICommand> = [];
+	var maxSize:Int;
 
-	public function new() {}
+	/**
+	 * @param maxSize The maximum number of commands to keep in the undo history.
+	 */
+	public function new(maxSize = 20) {
+		this.maxSize = maxSize;
+	}
 
 	public function execute(cmd:ICommand) {
 		cmd.execute();
-		undoStack.push(cmd);
+		addToUndoStack(cmd);
 		redoStack = [];
 	}
 
@@ -19,6 +25,9 @@ class History {
 			var c = undoStack.pop();
 			c.undo();
 			redoStack.push(c);
+			if (redoStack.length > maxSize) {
+				redoStack.shift();
+			}
 		}
 	}
 
@@ -26,12 +35,19 @@ class History {
 		if (redoStack.length > 0) {
 			var c = redoStack.pop();
 			c.execute();
-			undoStack.push(c);
+			addToUndoStack(c);
 		}
 	}
 
-	public function clear(){
+	public function clear() {
 		undoStack = [];
 		redoStack = [];
+	}
+
+	private function addToUndoStack(cmd:ICommand) {
+		undoStack.push(cmd);
+		if (undoStack.length > maxSize) {
+			undoStack.shift(); // Removes the oldest command (index 0)
+		}
 	}
 }
--- src/core/commands/RemoveConnectionsCommand.hx
+++ src/core/commands/RemoveConnectionsCommand.hx
@@ -2,34 +2,42 @@ package core.commands;
 
 import data.ConnectionData;
 
+// a small structure to track both the item and where it lived
+typedef RemovedConnection = {
+    var index:Int;
+    var data:ConnectionData;
+}
+
 class RemoveConnectionsCommand implements ICommand {
-	var graph:Graph;
-	var connections:Array<ConnectionData>;
-	var indeces:Array<Int>;
+    var graph:Graph;
+    var targets:Array<ConnectionData>;
+    var removedRecords:Array<RemovedConnection>;
 
-	public function new(graph:Graph, connections:Array<ConnectionData>) {
-		this.graph = graph;
-		this.connections = connections;
-		this.indeces = [];
-	}
+    public function new(graph:Graph, connections:Array<ConnectionData>) {
+        this.graph = graph;
+        this.targets = connections.copy();
+        this.removedRecords = [];
+    }
 
-	public function execute():Void {
-		for (connection in connections){
-			var index = graph.data.connections.indexOf(connection);
-			if (index != -1) {
-				graph.data.connections.splice(index, 1);
-			}
-			indeces.push(index);
-		}
-	}
+    public function execute():Void {
+        removedRecords = [];
 
-	// TODO: bug check this
-	public function undo():Void {
-		for(i in 0...connections.length){
-			var index = indeces[i];
-			if (indeces[i] >= 0) {
-				graph.data.connections.insert(index, connections[i]);
-			}
-		}
-	}
+        for (connection in targets) {
+            var index = graph.data.connections.indexOf(connection);            
+            // only act if the connection actually exists in the graph
+            if (index != -1) {
+                removedRecords.push({ index: index, data: connection });
+                graph.data.connections.splice(index, 1);
+            }
+        }
+    }
+
+    public function undo():Void {
+        var i = removedRecords.length - 1;
+        while (i >= 0) {
+            var record = removedRecords[i];
+            graph.data.connections.insert(record.index, record.data);
+            i--;
+        }
+    }
 }
